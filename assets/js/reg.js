bed5028b4c312cb323a4ba868f8b6079c22acaad
ushuz <hszqqq12@gmail.com>
2013-08-23 09:19:06 +0800

    check dom existence before binding events

--- assets/js/reg.js
+++ assets/js/reg.js
@@ -31,19 +31,23 @@ var checkForm = function() {
 // Binding events with functions.
 var uc = document.getElementById("uc");
 var mc = document.getElementById("mc");
+if (uc && mc) {
+    uc.onkeypress = digitOnly;
+    mc.onkeypress = digitOnly;
+}
 var form = document.getElementsByTagName("form")[0];
-uc.onkeypress = digitOnly;
-mc.onkeypress = digitOnly;
-form.onsubmit = checkForm;
-form.onkeyup = function() {
-    btn = document.getElementsByClassName("btn")[0];
-    if (checkForm()) {
-        if (btn.className.indexOf("btnValid") == -1) {
-            btn.className += " btnValid";
+if (form) {
+    form.onsubmit = checkForm;
+    form.onkeyup = function() {
+        btn = document.getElementsByClassName("btn")[0];
+        if (checkForm()) {
+            if (btn.className.indexOf("btnValid") == -1) {
+                btn.className += " btnValid";
+            }
+        } else {
+            if (btn.className.indexOf("btnValid") != -1) {
+                btn.className = btn.className.replace("btnValid", "");
+            }
         }
-    } else {
-        if (btn.className.indexOf("btnValid") != -1) {
-            btn.className = btn.className.replace("btnValid", "");
-        }
-    }
-};
+    };
+}
