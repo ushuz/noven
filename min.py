c4d6f31ac99cac3bbf54d63b29233ac26f3911c8
ushuz <hszqqq12@gmail.com>
2013-09-30 23:57:29 +0800

    min.py now needs directories as arguments

--- min.py
+++ min.py
@@ -9,15 +9,22 @@ if len(sys.argv) == 3:
     in_path = os.path.join(os.getcwd(), sys.argv[1])
     out_path = os.path.join(os.getcwd(), sys.argv[2])
 
-    with open(in_path) as f:
-        t = f.read().decode("utf-8")
+    dir, _, files = os.walk(in_path).next()
 
-    if in_path.endswith(".js"):
-        t = requests.post("http://marijnhaverbeke.nl/uglifyjs", data={"js_code": t}).text
-    else:
-        t = requests.post("http://cssminifier.com/raw", data={"input": t}).text
+    for i in files:
+        in_name = os.path.join(dir, i)
+        out_name = os.path.join(out_path, i)
+        with open(in_name) as f:
+            t = f.read().decode("utf-8")
 
-    with open(out_path, "w+") as f:
-        f.write(t.encode("utf-8"))
+        if in_name.endswith(".js"):
+            t = requests.post("http://marijnhaverbeke.nl/uglifyjs", data={"js_code": t}).text
+        else:
+            t = requests.post("http://cssminifier.com/raw", data={"input": t}).text
+
+        with open(out_name, "w+") as f:
+            f.write(t.encode("utf-8"))
+
+        print "Minimizing  %s" % os.path.join(sys.argv[2], i)
 else:
     print "usage: python min.py input_filename output_filename"
